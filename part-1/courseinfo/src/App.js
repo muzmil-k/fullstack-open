33a93d56e9ec45a0cc417454e59b95c44bfba787
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-17 06:41:42 +0500

    course information, step1

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header name={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
 
 export default App;
