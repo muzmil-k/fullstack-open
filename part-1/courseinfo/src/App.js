7b3d3b082496794a84cd9e3c9d17421cdf35b36b
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-23 23:39:16 +0500

    Course information step6

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -1,33 +1,28 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
-
+import Course from "./Course";
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3,
+            },
+        ],
+    };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+    return <Course course={course} />;
 };
 
 export default App;
--- part-1/courseinfo/src/Content.js
+++ part-1/courseinfo/src/Content.js
@@ -1,12 +1,16 @@
 import Part from "./Part";
 const Content = (props) => {
-  return (
-    <>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  );
+    return (
+        <>
+            {props.parts.map((part) => (
+                <Part
+                    key={part.id}
+                    name={part.name}
+                    exercises={part.exercises}
+                />
+            ))}
+        </>
+    );
 };
 
 export default Content;
