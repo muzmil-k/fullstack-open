6b910f37074bf163b86a7ef4262ff933c906de04
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-30 20:06:26 +0500

    Phonebook backend step3

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -32,6 +32,15 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (person) {
+        return response.json(person);
+    }
+    response.status(404).end();
+});
+
 app.get("/info", (request, response) => {
     const date = new Date();
     response.send(
