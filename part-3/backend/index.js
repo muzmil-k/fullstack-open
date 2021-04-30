ef9145e40a208e5c476e7f1e8efbbc514064224e
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-30 18:16:55 +0500

    part3-notes-a

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
+};
+app.use(requestLogger);
 
 let notes = [
     {
@@ -31,6 +41,51 @@ app.get("/api/notes", (request, response) => {
     response.json(notes);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const note = notes.find((note) => note.id === id);
+    if (note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter((note) => note.id !== id);
+    response.status(204).end();
+});
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+    return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+    const body = request.body;
+    if (!body.content) {
+        return response.status(400).json({ error: "content missing" });
+    }
+
+    const note = {
+        id: generateId(),
+        content: "body.content",
+        important: body.important || false,
+        date: new Date(),
+    };
+
+    notes = notes.concat(note);
+    response.json(note);
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
