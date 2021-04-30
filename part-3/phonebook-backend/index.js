a1162751f32aa9a0720ff27368b927c1108492fc
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-30 19:50:39 +0500

    Phonebook backend step2

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -32,6 +32,13 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+    const date = new Date();
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p> <p>${date}</p>`
+    );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
