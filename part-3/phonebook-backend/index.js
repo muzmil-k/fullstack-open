ae8f7dcfb9a8c6ac690ee4601405c6f6892f6cc2
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-04-30 20:14:51 +0500

    Phonebook backend step4

--- part-3/phonebook-backend/index.js
+++ part-3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
     {
         name: "Arto Hellas",
         number: "040-123456",
@@ -39,6 +39,12 @@ app.get("/api/persons/:id", (request, response) => {
         return response.json(person);
     }
     response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter((person) => person.id !== id);
+    response.status(204).end();
 });
 
 app.get("/info", (request, response) => {
