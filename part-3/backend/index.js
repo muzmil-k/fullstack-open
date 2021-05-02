43ffcba9ba2882e53e28f737f98e669918a3a9c4
Muzamil Khan <muzamilkhan5154@gmail.com>
2021-05-02 20:55:28 +0500

    heroku deploy

--- part-3/backend/index.js
+++ part-3/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 
 const requestLogger = (request, response, next) => {
@@ -85,7 +86,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
